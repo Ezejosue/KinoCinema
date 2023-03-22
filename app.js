8aa03cf35cde661437f4c2a5642077d2f4cef961
Josué Ávalos <aezequiel56@gmail.com>
2023-03-21 22:01:24 -0600

    added search engine

--- app.js
+++ app.js
@@ -1,34 +1,54 @@
 const API_KEY = "4b936bd5a7fe3d6313f58aff477d602f"; // Reemplaza esto con tu propia clave API de TMDB.
-const API_URL = "https://api.themoviedb.org/3/movie/now_playing";
-const IMG_URL = "https://image.tmdb.org/t/p/w500";
+const BASE_URL = "https://api.themoviedb.org/3";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
-const moviesContainer = document.getElementById("movies");
+document.getElementById("search-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const query = document.getElementById("search-input").value;
+  searchMovies(query);
+});
 
-fetch(`${API_URL}?api_key=${API_KEY}&language=es-ES&page=1`)
-  .then((res) => res.json())
-  .then((data) => {
-    displayMovies(data.results.slice(0, 6));
-  });
+async function searchMovies(query) {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
+  );
+  const data = await response.json();
+  displayResults(data.results);
+}
 
-  
-function displayMovies(movies) {
-  moviesContainer.innerHTML = movies
-    .map((movie) => {
-      return `
-            <div class="col-md-4 mt-4">
-                <div class="card">
-                    <img src="${
-                      IMG_URL + movie.poster_path
-                    }" class="card-img-top" alt="${movie.title}">
-                    <div class="card-body">
-                        <h5 class="card-title">${movie.title}</h5>
-                        <p class="card-text">${movie.overview.substr(
-                          0,
-                          100
-                        )}...</p>
-                    </div>
-                </div>
-            </div>`;
-    })
-    .join("");
+function displayResults(movies) {
+  const resultsDiv = document.getElementById("results");
+  resultsDiv.innerHTML = "";
+
+  for (const movie of movies) {
+    const movieCard = createMovieCard(movie);
+    resultsDiv.appendChild(movieCard);
+  }
+}
+
+function createMovieCard(movie) {
+  const card = document.createElement("div");
+  card.className = "card col-md-4 col-lg-3 mb-4 my-2";
+  const imagePath = movie.poster_path
+    ? `${IMAGE_BASE_URL}${movie.poster_path}`
+    : "https://via.placeholder.com/300x450?text=No+Image+Available";
+
+  const cardContent = `
+    <img src="${imagePath}" class="card-img-top" alt="${movie.title}">
+    <div class="card-body">
+      <h5 class="card-title">${movie.title}</h5>
+      <p class="card-text">Fecha de lanzamiento: ${
+        movie.release_date || "N/A"
+      }</p>
+      <p class="card-text">Calificación: ${movie.vote_average || "N/A"}</p>
+      <p class="card-text">${
+        movie.overview
+          ? movie.overview.slice(0, 100) + "..."
+          : "No hay descripción disponible."
+      }</p>
+    </div>
+  `;
+
+  card.innerHTML = cardContent;
+  return card;
 }
